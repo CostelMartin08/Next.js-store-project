d83301792b517675204e127433069b556d4d6e32
Costel <costelmartinescu2000@gmail.com>
2024-04-11 10:36:21 +0300

    ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "GadgetGrid",
+        script: "npm",
+        args: "start",
+        env: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
+  
