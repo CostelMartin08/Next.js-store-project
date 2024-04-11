d6b1abbbe4c27e87826d58bbbc38961c85d9e528
Costel <costelmartinescu2000@gmail.com>
2024-04-11 14:36:23 +0300

    v5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       cwd: "/home/node/GadgetGrid/Next.js-store-project",
       env: {
         NODE_ENV: "production",
-        PORT: 3001,
+        PORT: 8080,
       },
     },
   ],
