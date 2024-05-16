c2bd0daec9cd3649145cdf1886e2f42220223bf0
Costel <costelmartinescu2000@gmail.com>
2024-05-16 11:03:14 +0300

    promo final

--- app/usStory/page.tsx
+++ app/usStory/page.tsx
@@ -19,11 +19,11 @@ export default function UsStory() {
         if (window.innerWidth >= 768) {
             setPoster1('https://gadgetgrid.ro/poster/loginPromoLg.png');
             setPoster2('https://gadgetgrid.ro/poster/addProductPromoLg.png');
-            setPoster3('https://gadgetgrid.ro/poster/loginPromoLg.png');
+            setPoster3('https://gadgetgrid.ro/poster/posterLg.png');
         } else {
             setPoster1('https://gadgetgrid.ro/poster/loginPromoSm.png');
             setPoster2('https://gadgetgrid.ro/poster/addProductPromoSm.png');
-            setPoster3('https://gadgetgrid.ro/poster/loginPromoSm.png');
+            setPoster3('https://gadgetgrid.ro/poster/posterSm.png');
         }
     };
 
@@ -46,7 +46,7 @@ export default function UsStory() {
 
             <section className="container mt-5">
 
-                <div className="px-6">
+                <div className="sm:px-6">
 
                     <div style={{ backgroundColor: '#1B2123' }} className="flex  items-center rounded-md justify-center">
 
@@ -61,9 +61,10 @@ export default function UsStory() {
                     </div>
 
 
-                    <div className='bg-white p-8 md:p-14 text-left md:text-center flex flex-col items-center gap-8 rounded-md mt-10'>
+                    <div className='bg-white p-4 md:p-14 text-left md:text-center flex flex-col items-center gap-8 rounded-md mt-10'>
 
                         <h4 className="text-4xl font-black">What is GadgetGrid?</h4>
+
                         <p className="md:w-[45rem] text-sm md:text-lg">
                             GadgetGrid is a web application that I created entirely in two months,
                             using the Next.js framework and TypeScript. This project gave me the
@@ -88,10 +89,10 @@ export default function UsStory() {
                     </div>
 
 
-                    <div className="flex flex-col items-center md:place-items-start md:flex-row gap-8 bg-white p-8 md:p-14 rounded-md mt-10">
+                    <div className="flex flex-col items-center md:place-items-start md:flex-row gap-8 bg-white p-4 md:p-14 rounded-md mt-10">
 
                         <Image
-                            className="rounded-md"
+                            className="w-full rounded-md"
                             src={Auth}
                             height={300}
                             width={300}
@@ -99,9 +100,9 @@ export default function UsStory() {
 
                         </Image>
 
-                        <div className="text-left md:text-lg space-y-10 text-left md:text-justify">
+                        <div className="space-y-10 text-left md:text-justify">
 
-                            <p>
+                            <p className="md:w-[45rem] text-sm md:text-lg">
                                 Auth.js is a JavaScript library for authentication and authorization,
                                 which facilitates the implementation of authentication functionalities
                                 in web applications. Within this project, we have opted for both
@@ -109,13 +110,13 @@ export default function UsStory() {
                                 the target audience of this application.
                             </p>
 
-                            <p>
+                            <p className="md:w-[45rem] text-sm md:text-lg">
                                 GadgetGrid offers a straightforward authentication experience.
                                 From email address verification to two-factor authentication
                                 and password reset.
                             </p>
 
-                            <p>
+                            <p className="md:w-[45rem] text-sm md:text-lg">
                                 I would like to mention that GadgetGrid is a demonstrative application
                                 without any economic purpose. The collected data, specifically email
                                 addresses and profile photos, are deleted upon request.
@@ -126,7 +127,7 @@ export default function UsStory() {
 
                     <div className="mt-10">
 
-                        <h5 className="text-3xl md:text-6xl font-black">The authentication process</h5>
+                        <h5 className="text-2xl md:text-5xl font-black">The authentication process</h5>
 
                     </div>
 
@@ -144,20 +145,20 @@ export default function UsStory() {
                     </div>
 
 
-                    <div className="bg-white flex flex-col-reverse justify-center items-center md:flex-row gap-8 rounded-md mt-10 p-8 md:p-14 ">
+                    <div className="bg-white flex flex-col-reverse justify-center items-center md:flex-row gap-8 rounded-md mt-10 p-4 md:p-14">
 
-                        <div className="text-left md:text-lg space-y-8 text-left md:text-justify">
+                        <div className="space-y-8 text-left md:text-justify">
 
-                            <p>
+                            <p className="md:w-[45rem] text-sm md:text-lg">
                                 Inspired by the e-commerce platform Shopify, we have developed a similar
                                 system for managing products, aiming to provide an efficient and intuitive
                                 experience to users.
                             </p>
-                            <p>
+                            <p className="md:w-[45rem] text-sm md:text-lg">
                                 Through this system, users with administrator roles
                                 are empowered to add new products to the catalog and modify existing ones.
                             </p>
-                            <p>
+                            <p className="md:w-[45rem] text-sm md:text-lg">
                                 This functionality enables flexible management of the entire inventory,
                                 including updating descriptions, enhancing images, and adjusting prices
                                 to meet the ever-changing market demands and preferences.
@@ -168,7 +169,7 @@ export default function UsStory() {
 
                         <Image
                             quality={100}
-                            className="rounded-md"
+                            className="w-full rounded-md"
                             src={Admin}
                             width={300}
                             height={300}
@@ -180,7 +181,7 @@ export default function UsStory() {
 
                     <div className="mt-10">
 
-                        <h5 className="text-3xl md:text-6xl font-black">Add products</h5>
+                        <h5 className="text-2xl md:text-5xl font-black">Add products</h5>
 
                     </div>
 
@@ -200,7 +201,7 @@ export default function UsStory() {
 
                     <div className="mt-10">
 
-                        <h5 className="text-3xl md:text-6xl font-black">Inventory management</h5>
+                        <h5 className="text-2xl md:text-5xl font-black">Inventory management</h5>
 
                     </div>
 
