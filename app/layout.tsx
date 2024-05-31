490353849698b4cbabd60eab00eefc4b78b7812e
Costel <costelmartinescu2000@gmail.com>
2024-05-31 18:13:06 +0300

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,10 @@ import "./globals.css";
 import Footer from './components/top&bottom/Footer';
 import Head from 'next/head';
 
+import { GoogleAnalytics } from './lib/ga';
+
+import Script from 'next/script';
+
 const lato = Lato({
   weight: ['100', '300', '400', '700', '900'],
   style: ['normal', 'italic'],
@@ -45,25 +49,29 @@ export default async function RootLayout({
     <SessionProvider session={session}>
 
       <html lang="en">
-        <Head>
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
-        </Head>
+    
         <body className={lato.className} >
 
           <AppWrapper>
-
+          <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              strategy="afterInteractive" 
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+           {/* <GoogleAnalytics />*/}
             {children}
             <Footer />
 
